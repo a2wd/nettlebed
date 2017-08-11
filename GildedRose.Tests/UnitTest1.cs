03dc0799b6803393787ec20c046ec91fb5d8e8eb
Alito Alvarez <Alito.alvarez@asos.com>
2017-08-11 16:03:50 +0100

    Add project files.

---
+++ GildedRose.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRose.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
