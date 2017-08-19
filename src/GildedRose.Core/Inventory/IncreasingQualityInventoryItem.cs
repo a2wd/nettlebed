0d13d875d5725bab35c84b7846f22a2d36f23e89
a2wd <github@a2wd.com>
2017-08-19 19:42:04 +0100

    Updated templates to include increasing value items

---
+++ src/GildedRose.Core/Inventory/IncreasingQualityInventoryItem.cs
@@ -0,0 +1,17 @@
+﻿namespace GildedRose.Core.Inventory
+{
+    public class IncreasingQualityInventoryItem : InventoryItem
+    {
+        public IncreasingQualityInventoryItem(Item item) : base(item)
+        {
+        }
+
+        protected override void UpdateQuality()
+        {
+            if (_item.Quality < Constants.Values.MaxQuality)
+            {
+                _item.Quality = _item.Quality + Constants.Rates.Quality;
+            }
+        }
+    }
+}
